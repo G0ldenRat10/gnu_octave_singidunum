9a14ee9136e2f762306a033b1cfac54e95f8884e
Lazar Tatomir <lazar.tatomir.25@singimail.rs>
2026-01-02 18:48:33 +0100

    Add files via upload

--- Book_Examples/vezba_10.m
+++ Book_Examples/vezba_10.m
@@ -1,44 +1,73 @@
-# Uneti ovo:
-# pkg load symbolic
+# Kramerova Metoda
+# 2x - 4y -z = 0
+# -x + 2y + 2z = 6
+# 3x + 6y      = 6
 
-# 1.
-# Решити једначину x^2 - 2*x = 0
+A = [2,-4,-1;-1,2,2;3,6,0];
+B = [0;6;6];
 
-syms x
-resenje = solve(x^2 - 2*x == 0, x);
-printf('Resenja:\n');
-disp(resenje);
+[m,n] = size(A); # 3x3 -> m = 3, n = 3
+if m ~= n
+  printf('Matrica nije kvadrartna');
+elseif det(A) == 0
+  printf('Matrica je singularna');
+else
+  for j = 1:n
+    C = A;
+    C(:,j) = B;
+    X(j) = det(C) / det(A)
+  endfor
+  X = X'; # transponovano iz reda u kolonu
+end
+printf('Rezultat Kramerove metode je:\n');
+disp(X);
+
+
+# Matricna Metoda
+# -2x + 3y + z = 1
+# x + 3y - 2z = 1
+#  x - 6y + z = 3
+
+A = [-2,3,1;1,3,-2;1,-6,1];
+B = [1;1;3];
+
+if det(inv(A)) == 0
+  printf('Inverzna matrica ne postoji.\n');
+else
+  X = inv(A) * B;
+end
+printf('Resenje je: \n');
+disp(X);
+
+#1.
+# Решити једначину x^4 - 2x^2 + 1 = 0
+# f(x) = 0
+# fplot('x^4 - 2 * x^2 + 1',[0 8]);
+# x = fzero('x^4 - 2 * x^2 + 1',0);
 
 # 2.
-# Resiti jednacinu a*x^2 + 5*b*x - 2 = 0
-
-syms a b x
-resenje = solve(a * x^2 + 5 * b * x -2,a,b,x);
-printf('Resenja: \n');
-disp(resenje);
-
-# 3.
-# Resiti sistem jednacina 2x - y - 1 = 0 , i , x + 2y + 4 = 0
-syms x y
-resenje = solve(2*x - y - 1 == 0, x + 2*y + 4 == 0,x,y);
-printf('Resenja: \n');
-disp(resenje.x);
-disp(resenje.y);
+# Nacrtati grafik polinoma y = 0.02x^4 - 0.75x^3 + 12.5x - 2y u opsegu -6<= x <= 6
+P = [0.02 0.75 12.5 2];
+x = -6:0.1:6;
+y = polyval(P,x);
+plot(x,y);
 
 # 4.
-# Nacrtati funkciju y = e^x + 1 u razlicitim domenima
-syms x
-y = exp(1)^x + 1;
-n = sym(50);
-ezplot(y,n);
-
-# 5.
-# Ispitati uzajamni polozaj kruga x^2 + y^2 = 1 i pravih
-# a) x + y - 4 = 0
-# b) x + y - 1 = 0
-# c) x + y - sqrt(2) = 0
-# zadatak uraditi racunarski i graficki
-
-syms x y
-# a [x y] = solve(x^2 + y^2 == 1, x + y - 4 == 0,x,y);
-# isto tako za b i c
+# Cramer
+A = [1,3,-2;2,-1,1;1,0,2];
+B = [1;3;7];
+[m,n] = size(A);
+if m ~= n
+   printf('Nije kvadratna.');
+else if det(A) == 0
+   printf('Nema resenja.');
+else
+   for j = 1:n
+     C = A;
+     C(:,j) = B;
+     X{j) = det(C) / det(A);
+   endfor
+   X = X';
+end
+printf('Resenje:\n');
+disp(X);
